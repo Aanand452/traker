0658ab10b2f46dd4566e8683fd9a76fde022b06c
millergonzalez <mgonzalez@inkling.com>
2020-07-30 17:40:12 -0500

    no ignore build - demo porpuse

---
+++ build/precache-manifest.9bc1cff6824779934370ce7bad76e369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f56c1ba7325a4f841c80898b8c8156",
+    "url": "/index.html"
+  },
+  {
+    "revision": "499f073d6f14ce22a7e3",
+    "url": "/static/css/main.673c43fb.chunk.css"
+  },
+  {
+    "revision": "64cd95fb3ba8d8aebf21",
+    "url": "/static/js/2.12755a08.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.12755a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "499f073d6f14ce22a7e3",
+    "url": "/static/js/main.6f5ae476.chunk.js"
+  },
+  {
+    "revision": "8252c1694cbb4f4f0965",
+    "url": "/static/js/runtime-main.4d7a2c2e.js"
+  }
+]);
