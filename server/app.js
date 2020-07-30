25a72889b6bfb6afaa6760d4c37cc1e21733a956
millergonzalez <mgonzalez@inkling.com>
2020-07-30 17:01:02 -0500

    firts commit

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+var env = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send({});
+});
+
+app.set('port', 80);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, '../build')));
+app.use(express.static(path.resolve(__dirname, 'static')));
+
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+}); 
