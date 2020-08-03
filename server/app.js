da42f23e3a28979a9db3b345bcd45ff79c60f11d
millergonzalez <mgonzalez@inkling.com>
2020-08-03 08:44:42 -0500

    fix routes

--- server/app.js
+++ server/app.js
@@ -6,20 +6,23 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const env = process.env.NODE_ENV || 'development';
 
-console.log('selected server port: ', port);
 const app = express();
 
 app.get('/', function (req, res) {
   res.sendFile(path.resolve(__dirname + '/../build/index.html'));
 });
 
+app.get(['/admin', '/mandatory-1', '/mandatory-2', '/mandatory-3'], function (req, res) {
+  res.sendFile(path.resolve(__dirname + '/../build/index.html'));
+});
+
+const static = express.static(path.resolve(__dirname, '../build'));
+
 app.set('port', port);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.resolve(__dirname, '../build')));
-app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(static);
 
 app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
