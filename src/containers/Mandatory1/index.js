0c06e50c2c110396a040e8b47a67b9f16f8d80c8
Josias Ortiz Garcia <josias.ortiz@globant.com>
2020-08-07 12:31:35 -0500

    form create new row

--- src/containers/Mandatory1/index.js
+++ src/containers/Mandatory1/index.js
@@ -1,51 +1,193 @@
 import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+
 import {
   IconSettings,
-  Dropdown,
-  DropdownTrigger,
+  Combobox,
+  Input,
+  Datepicker,
   Button
 } from '@salesforce/design-system-react';
 
+import { theme, program, format, persona, region } from '../../utils/data';
+
 const Mandatory = () => {
+  const [row, setRow] = useState({});
+  const [selection, setSelection] = useState([]);
+
+  const handleCHange = (value, data) => setRow({...row, [value]: data});
+
+  const themeModel = theme.map((el, index)=>{
+    el.label = el.text
+    el.id = index.toString();
+    return el
+  })
+  
+  const programModel = program.map((el, index)=>{
+    el.label = el.text
+    el.id = index.toString();
+    return el
+  })
+
+  const formatModel = format.map((el, index)=>{
+    el.label = el.text
+    el.id = index.toString();
+    return el
+  })
+
+  const personaModel = persona.map((el, index)=>{
+    el.label = el.text
+    el.id = index.toString();
+    return el
+  })
+
+  const regionModel = region.map((el, index)=>{
+    el.label = el.text
+    el.id = index.toString();
+    return el
+  })
 
     return (
-      <section class="slds-grid slds-wrap">
-        <div class="slds-grid slds-wrap slds-p-around_medium slds_full-width">
-          <div class="slds-col slds-size_12-of-12">
+      <section className="slds-grid slds-wrap background-white">
+        <div className="slds-grid slds-wrap slds-p-around_medium slds_full-width">
+          <div className="slds-col slds-size_12-of-12">
             <div className="slds-admin-view">
               <form >
-              <IconSettings iconPath="assets/icons">
-                <Dropdown
-                  tabIndex="-1"
-                  align="right"
-                  options={[
-                    { label: 'Menu Item One', value: 'A0' },
-                    { label: 'Menu Item Two', value: 'B0' },
-                    { label: 'Menu Item Three', value: 'C0' },
-                    { type: 'divider' },
-                    { label: 'Menu Item Four', value: 'D0' },
-                    { label: 'Menu Item Five', value: 'E0' },
-                    { label: 'Menu Item Six', value: 'F0' },
-                    { type: 'divider' },
-                    { label: 'Menu Item Seven', value: 'G0' },
-                  ]}
-                >
-                  <DropdownTrigger>
-                    <Button
-                      iconCategory="utility"
-                      iconName="down"
-                      iconPosition="right"
-                      label="Artificial Intelligence"
-                    />
-                  </DropdownTrigger>
-                </Dropdown>
-              </IconSettings>
+                <IconSettings iconPath="assets/icons">
+                  <Combobox
+                    id="theme"
+                    events={{
+                      onSelect: (event, data) => {data.selection.length ? handleCHange("theme", data.selection) : null}
+                    }}
+                    labels={{
+                      label: 'Theme'
+                    }}
+                    name="theme"
+                    options={themeModel}
+                    selection={row.theme}
+                    value={"theme"}
+                    variant="readonly"
+                  />
+                </IconSettings>
+                <IconSettings iconPath="assets/icons">
+                  <Combobox
+                    id="program"
+                    events={{
+                      onSelect: (event, data) => {data.selection.length ? handleCHange("program", data.selection) : null}
+                    }}
+                    labels={{
+                      label: 'Program'
+                    }}
+                    name="program"
+                    options={programModel}
+                    selection={row.program}
+                    value={"program"}
+                    variant="readonly"
+                  />
+                </IconSettings>
+                <Input onChange={(event, data) => {handleCHange("title", data.value)}} defaultValue={row.title} id="title" label="Title" className="slds-form-element__create-row"/>
+                <IconSettings iconPath="assets/icons">
+                  <Combobox
+                    id="format"
+                    events={{
+                      onSelect: (event, data) => {data.selection.length ? handleCHange("format", data.selection) : null}
+                    }}
+                    labels={{
+                      label: 'Format'
+                    }}
+                    name="format"
+                    options={formatModel}
+                    selection={row.format}
+                    value={"format"}
+                    variant="readonly"
+                  />
+                </IconSettings>
+                <IconSettings iconPath="assets/icons">
+                  <Combobox
+                    id="persona"
+                    events={{
+                      onSelect: (event, data) => {data.selection.length ? handleCHange("persona", data.selection) : null}
+                    }}
+                    labels={{
+                      label: 'Persona'
+                    }}
+                    name="persona"
+                    options={personaModel}
+                    selection={row.persona}
+                    value={"persona"}
+                    variant="readonly"
+                  />
+                </IconSettings>
+                <Input onChange={(event, data) => {handleCHange("abstract", data.value)}} id="abstract" label="Abstract" className="slds-form-element__create-row"/>
+                <IconSettings iconPath="assets/icons">
+                  <Combobox
+                    id="region"
+                    events={{
+                      onSelect: (event, data) => {data.selection.length ? handleCHange("region", data.selection) : null}
+                    }}
+                    labels={{
+                      label: 'Region'
+                    }}
+                    name="region"
+                    options={regionModel}
+                    selection={row.region}
+                    value={"region"}
+                    variant="readonly"
+                  />
+                </IconSettings>
+                <IconSettings iconPath="assets/icons">
+                <Datepicker
+                  id="startDate"
+                  labels={{
+                    label: 'Start Date',
+                  }}
+                  triggerClassName="slds-form-element__create-row"
+                  onChange={(event, data) => {
+                    console.log(event.target)
+                    console.log(data)
+                    setRow({...row, ["startDate"]: data.date})
+                  }}
+                  onCalendarFocus={(event, data) => {
+                    
+                  }}
+                  formatter={(date) => {
+                    return date ? moment(date).format('YYYY/MM/DD') : '';
+                  }}
+                  parser={(dateString) => {
+                    return moment(dateString, 'YYYY-MM-DD').toDate();
+                  }}
+                  value={row.startDate}
+                />
+                </IconSettings>
+                <IconSettings iconPath="assets/icons">
+                <Datepicker
+                  id="endDate"
+                  labels={{
+                    label: 'Start Date',
+                  }}
+                  triggerClassName="slds-form-element__create-row"
+                  onChange={(event, data) => {
+                    console.log(event.target)
+                    console.log(data)
+                    setRow({...row, ["endDate"]: data.date})
+                  }}
+                  onCalendarFocus={(event, data) => {
+                    
+                  }}
+                  formatter={(date) => {
+                    return date ? moment(date).format('YYYY/MM/DD') : '';
+                  }}
+                  parser={(dateString) => {
+                    return moment(dateString, 'YYYY-MM-DD').toDate();
+                  }}
+                  value={row.endDate}
+                />
+                </IconSettings>
+                <Input onChange={(event, data) => {handleCHange("result", data.value)}} id="result" label="Result" className="slds-form-element__create-row"/>
+                <Input onChange={(event, data) => {handleCHange("asset", data.value)}} id="asset" label="Asset" className="slds-form-element__create-row"/>
+                <Button label="Cancel" />
+                <Button label="Create Row" variant="brand" />
               </form>
-              <ul className="slds-button-group-row slds-m-top_x-small">
-                <li className="slds-button-group-item">
-                  <button className="slds-button slds-button_brand">Upload CSV</button>
-                </li>
-              </ul>
             </div>
           </div>
         </div>
