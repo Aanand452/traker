693d03b7dd53410a776cf13ec5c074440e6ae3f7
millergonzalez <mgonzalez@inkling.com>
2020-08-10 11:59:39 -0500

    remove banner

--- src/containers/styles/page.js
+++ src/containers/styles/page.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
 const bgImg = ['./images/winter-background.png', './images/background.png'];
-
+//background: url(${bgImg[Math.round(Math.random(bgImg*1))]}) no-repeat;
 const SfdcPageAppWrapper = styled.div`
   background-color: #f4f6f9;
-  background: url(${bgImg[Math.round(Math.random(bgImg*1))]}) no-repeat;
+  
   background-size: contain;
   background-position: left 0;
   background-attachment: fixed;
