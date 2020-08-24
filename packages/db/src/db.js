c8c97ae6b8d4a9f7f12fedf7942404651f0b6ad6
millergonzalez <mgonzalez@inkling.com>
2020-08-24 16:06:58 -0500

    run script done

--- packages/db/src/db.js
+++ packages/db/src/db.js
@@ -2,21 +2,28 @@ import * as Sequelize from 'sequelize'
 import fs from 'fs';
 import config from '../config/config';
 
-
-//let raw = fs.readFileSync(__dirname + '/../config/config.js', 'utf8');
-//let config = JSON.parse(raw);
 let env = process.env.NODE_ENV || 'development'; 
 
 const db = 'db'
 const username = process.env.DB_PWD || config[env].username;
 const password = process.env.DB_USER || config[env].password
 const host = process.env.DB_HOST || config[env].host
-
-export const sequelize = new Sequelize.Sequelize(db, username, password, {
+const options = {
   dialect: "postgres",
   port: 5432,
   host: host || 'postgres'
-});
+}
+
+options = process.env.NODE_ENV && {options, ...{
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: false
+  }
+}}
+
+//console.log(options, db, username, password);
+
+export const sequelize = new Sequelize.Sequelize(db, username, password, options);
 
 sequelize.authenticate().then(data => {
   console.info('sucesss');
