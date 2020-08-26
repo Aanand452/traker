28df396399a322274e606252d4de1a4b49e57428
millergonzalez <mgonzalez@inkling.com>
2020-08-26 07:50:49 -0500

    fix babel envs

--- babel.config.js
+++ babel.config.js
@@ -14,17 +14,5 @@ module.exports = {
   presets,
   "ignore": [
     "node_modules"
-  ],
-  "plugins": [
-    [
-      "module-resolver",
-      {
-        "alias": {
-          "@sara/common/src": "@sara/common/build",
-          "@sara/db/src": "@sara/db/build",
-          "@sara/server/src": "@sara/server/build"
-        }
-      }
-    ]
   ]
 };
--- packages/db/src/db.js
+++ packages/db/src/db.js
@@ -4,24 +4,15 @@ import config from '../config/config';
 
 let env = process.env.NODE_ENV || 'development'; 
 
-const db = 'db'
-const username = process.env.DB_PWD || config[env].username;
-const password = process.env.DB_USER || config[env].password
+const db = process.env.DB_DB || 'db'
+const username = process.env.DB_USER || config[env].username;
+const password = process.env.DB_PWD || config[env].password
 const host = process.env.DB_HOST || config[env].host
-const options = {
+let options = {
   dialect: "postgres",
   port: 5432,
   host: host || 'postgres'
 }
-
-options = process.env.NODE_ENV && {options, ...{
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: false
-  }
-}}
-
-//console.log(options, db, username, password);
 
 export const sequelize = new Sequelize.Sequelize(db, username, password, options);
 
