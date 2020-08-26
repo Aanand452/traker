20e88cc80de68c2518ed30e5d9308e3414d058ce
millergonzalez <mgonzalez@inkling.com>
2020-08-26 08:00:29 -0500

    fix env vars

--- packages/db/config/config.js
+++ packages/db/config/config.js
@@ -16,7 +16,7 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: "postgres"
   },
-  production: {
+  stable: {
     username: process.env.DB_USER,
     password: process.env.DB_PWD,
     database: process.env.DB_DB,
