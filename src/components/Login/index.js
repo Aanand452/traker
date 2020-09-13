4b4dc266391ebb72b05994153f885ec0cca6ab5c
Juan Pablo Inzunza Alfaro <johnny.phocker@gmail.com>
2020-09-13 16:47:55 -0600

    Added error handlers

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -15,7 +15,8 @@ class Login extends Component {
   state = {
     username: '',
     password: '',
-    isToastOpened: false
+    isToastOpened: false,
+    errors: {}
   }
 
   componentDidMount() {
@@ -25,6 +26,23 @@ class Login extends Component {
   setupAndFetch = async () => {
     if(window.location.hostname === 'localhost') this.API_URL =  "http://localhost:3000/api/v1";
     else this.API_URL = await getAPIUrl();
+  }
+
+  validations = res => {
+    if(this.state.username === '') {
+      this.setState({errors: {username: true}});
+      return true;
+    }
+
+    if(this.state.password === '') {
+      this.setState({errors: {password: true}});
+      return true;
+    }
+
+    if(res.length <= 0) {
+      this.setState({errors: {invalid: true}});
+      return true;
+    }
   }
 
   login = async e => {
@@ -43,18 +61,26 @@ class Login extends Component {
       }
       let response = await fetch(`${this.API_URL}/login`, config);
       let { result } = await response.json();
+
+      if(this.validations(result)) return;
       
       localStorage.setItem("userId", result[0].userId);
       this.props.history.push('/home');
 
     } catch (err) {
       this.setState({isToastOpened: true});
-      console.error(err);
+      console.error(err.message);
     }
   }
 
   handleChange = e => {
-    this.setState({[e.target.name]: e.target.value});
+    let errors = {...this.state.errors}
+    if(e.target.value.length === 0) {
+      errors = { ...errors, [e.target.name]: true }
+    } else {
+      delete errors[e.target.name];
+    }
+    this.setState({[e.target.name]: e.target.value, errors});
   }
 
   render() {
@@ -75,25 +101,28 @@ class Login extends Component {
           <article className="slds-card">
             <SfdcCardLogin className="slds-card__body slds-card__body_inner">
               <form onSubmit={e => this.login(e)}>
+                {this.state.errors.invalid && <div className="slds-form-element slds-has-error">
+                  <div className="slds-form-element__help" id="error-message-unique-id">Invalid credentials</div>
+                </div>}
                 <div className="slds-form-element">
                   <Input
-                    required
                     placeholder="Enter username"
                     onChange={this.handleChange}
                     value={this.state.username}
                     name="username"
                     label="Username"
+                    errorText={this.state.errors.username && "This field is required"}
                   />
                 </div>
                 <div className="slds-form-element slds-m-top_small">
                   <Input
-                    required
-                    placeholder="Enter username"
+                    placeholder="Enter password"
                     onChange={this.handleChange}
                     value={this.state.password}
                     type="password"
                     name="password"
                     label="Password"
+                    errorText={this.state.errors.password && "This field is required"}
                   />
                 </div>
                 <button type="submit" className="slds-button slds-button_brand slds-button_stretch slds-m-top_small">Login</button>
