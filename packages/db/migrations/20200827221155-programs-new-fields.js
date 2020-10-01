2326dcbba6a283d09f8d8dc8571090b8cd50b9bd
Miller Gonzalez <millergonzalez@millergonz-ltm.internal.salesforce.com>
2020-10-01 08:55:25 -0500

    Migration to allow nulls in activity entity

--- packages/db/migrations/20200827221155-programs-new-fields.js
+++ packages/db/migrations/20200827221155-programs-new-fields.js
@@ -11,7 +11,7 @@ module.exports = {
     await queryInterface.addColumn(
       'program',
       'metrics',
-      Sequelize.TEXT
+      Sequelize.INTEGER
     );
 
     await queryInterface.addColumn(
