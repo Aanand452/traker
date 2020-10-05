a241c2e338f5737622e98f460736f860508a2dea
Miguel Velez <miguel.velez@globant.com>
2020-10-05 10:43:22 -0500

    Keep getAPIUrl

--- src/components/CreateActivity/Step1/index.js
+++ src/components/CreateActivity/Step1/index.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
+import { getAPIUrl } from '../../../config/config';
 import {
   Combobox,
   Button,
