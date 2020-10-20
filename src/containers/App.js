f1bfeb04a20af4fbcbaa986670826ccd1378bcd1
millergonzalez <mgonzalez@inkling.com>
2020-10-20 16:55:35 -0500

    cookie error fixed

--- src/containers/App.js
+++ src/containers/App.js
@@ -58,24 +58,6 @@ function CreateProgram() {
   );
 };
 
-function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-
-localStorage.setItem('userId', getCookie('userid'));
-
 function App({closeSettingsMenu, user}) {
   return (
     <Router>
--- src/index.js
+++ src/index.js
@@ -19,6 +19,25 @@ let store = createStore(app, composeEnhancers(
 const cookie = document.cookie.match(/(user=([^;]+))/);
 const user = cookie && cookie[2];
 
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+localStorage.setItem('userId', getCookie('userid').replaceAll('"',''));
+console.log('DEBUG', getCookie('userid').replaceAll('"',''))
+
 if (user) {
   const userObj = JSON.parse(decodeURIComponent(user));
   store.dispatch(setUser(userObj));
