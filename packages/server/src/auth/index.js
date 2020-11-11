a329b8a0c83d30014b749564f5b15d4398f44fdb
Miller Gonzalez <millergonzalez@salesforce.com>
2020-11-11 10:19:10 -0500

    basic JWT auth

---
+++ packages/server/src/auth/index.js
@@ -0,0 +1,25 @@
+import UserModel from '@sara/db/src/models/user';
+import jwt from 'jsonwebtoken';
+
+const sharedSecret = process.env.JWT_KEY || 'secret'
+
+const sendError = (req) => {
+  return req.res.status(403).json({message: 'Error: Access Denied'});
+}
+
+const validateToken = async (tokenString) => {
+  let decodedToken = jwt.verify(tokenString, sharedSecret)
+
+  const userId = await UserModel.getUserByEmail(decodedToken.username);
+  return userId && userId === decodedToken.user_id;
+}
+
+const verifyToken = async (req, authOrSecDef, token, callback)  => {
+  if (token && token.indexOf("Bearer ") === 0) {
+    await validateToken(token.split(' ')[1]) ? callback(null) : sendError(req);
+  } else {
+    sendError(req);
+  }
+}
+
+export { verifyToken };
