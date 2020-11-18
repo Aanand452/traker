338f7f700a87d0e9cb1ca8294574e0033faf3768
Jonathan Villamizar <jonathan.villamizar@globant.com>
2020-11-18 10:14:37 -0500

    replacing SSO app code

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ const config = {
     strategy: 'saml',
     saml: {
       path: process.env.SAML_PATH || '/login/callback',
-      entryPoint: process.env.SAML_ENTRY_POINT || 'https://mgonzalez-dev-ed.my.salesforce.com/idp/login?app=0sp3g0000008OIU',
+      entryPoint: process.env.SAML_ENTRY_POINT || 'https://mgonzalez-dev-ed.my.salesforce.com/idp/login?app=0sp3g0000008OIt',
       issuer: 'Activity_tracker_qa',
       cert: process.env.SAML_CERT || null
     }
