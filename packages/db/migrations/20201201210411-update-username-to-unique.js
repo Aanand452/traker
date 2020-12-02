e15414a5ce288bc8ad267fdef90a73396c6b8fe4
Jonathan Villamizar <jonathan.villamizar@globant.com>
2020-12-02 12:28:07 -0500

    fixing migration

--- packages/db/migrations/20201201210411-update-username-to-unique.js
+++ packages/db/migrations/20201201210411-update-username-to-unique.js
@@ -3,12 +3,14 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.changeColumn('user', 'username', {
+      type: Sequelize.STRING,
       unique: true
     });
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.changeColumn('user', 'username', {
+      type: Sequelize.STRING,
       unique: false
     });
   }
