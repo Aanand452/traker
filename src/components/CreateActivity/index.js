a3234e1d428913285b6278c1907147991f24df53
Juan Pablo Inzunza Alfaro <juan.inzunza@globant.com>
2020-12-03 09:42:05 -0700

    Fixed warnings in console

--- src/components/CreateActivity/index.js
+++ src/components/CreateActivity/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
 import {
   IconSettings,
   ToastContainer,
@@ -355,4 +354,4 @@ class CreateActivity extends Component {
   };
 };
 
-export default withRouter(CreateActivity);
+export default CreateActivity;
