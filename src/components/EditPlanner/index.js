d4d9422c14bcfc1a65685cb25206d4faa276596e
Krishna Verma <kvarma@kvarma-ltm.internal.salesforce.com>
2021-10-12 07:56:05 +0530

    refine planning view css

--- src/components/EditPlanner/index.js
+++ src/components/EditPlanner/index.js
@@ -3,6 +3,8 @@ import moment from 'moment';
 import NavBar from '../NavBar';
 import BudgetInput from '../BudgetInput/BudgetInput'
 import update from 'immutability-helper';
+import {BoxShadow} from './styles'
+import './styles.css'
 
 import {
     Icon,
@@ -37,10 +39,51 @@ class EditPlanner extends Component {
 
 
     render() {
-        return (<div>
-            <div style={{fontSize:'50px'}}>FY22 H2 Master Program Plan: ESMB(ANZ)</div>
-            <div style={{backgroundColor:'blue', width:'15%', fontSize:'20px', color:'white', paddingLeft:'20px', borderRadius:'0 20px 20px 0'}}>Owner</div>
-        </div>);
+        return (
+        // <div>
+        //     <div style={{fontSize:'50px'}}>FY22 H2 Master Program Plan: ESMB(ANZ)</div>
+        //     <div style={{backgroundColor:'blue', width:'15%', fontSize:'20px', color:'white', paddingLeft:'20px', borderRadius:'0 20px 20px 0'}}>Owner</div>
+        //     <div style={{fontSize:'20px', paddingLeft:'20px'}}>Cat Prestipino</div>
+        //     <div style={{paddingLeft:'20%', paddingBottom:'80%'}}><BoxShadow>Hi</BoxShadow></div>
+        // </div>
+        <div class="grid-container">
+        <div class="item1">FY22 H2 Master Program Plan: ESMB(ANZ)</div>
+        <div class="item2">
+            <div className="owner">Owner</div>
+            <div className="owner-name">Cat Prestipino</div>
+
+            <div className="owner">Owner</div>
+            <div className="owner-name">Cat Prestipino</div>
+        </div>
+        <div class="item3">
+            <div className="card">
+                <div className="card-head">MP Target : 
+                    <div className="card-head-value">S8M</div>
+                </div>
+                <hr className="hr1"/>
+            </div>
+            <div className="card">
+                <div className="card-head">MP Target : 
+                    <div className="card-head-value">S8M</div>
+                </div>
+                <hr className="hr1"/>
+            </div>
+            <div className="card">
+                <div className="card-head">MP Target : 
+                    <div className="card-head-value">S8M</div>
+                </div>
+                <hr className="hr1"/>
+            </div>
+            <div className="card">
+                <div className="card-head">MP Target : 
+                    <div className="card-head-value">S8M</div>
+                </div>
+                <hr className="hr1"/>
+            </div>
+        </div> 
+        <div class="item5">Footer</div>
+        </div>
+        );
     }
 };
 
---
+++ src/components/EditPlanner/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const BoxShadow = styled.div`
+  width:250px;
+  padding-top:30px;
+  box-shadow: 10px 5px 5px gray`;
+
+
+export {BoxShadow}
