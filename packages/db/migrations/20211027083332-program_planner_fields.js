c9d078a38c2eadd10b93d9fbaa1b8d8af58f745c
Krishna Verma <kvarma@kvarma-ltm.internal.salesforce.com>
2021-10-27 15:52:15 +0530

    fix migrations

--- packages/db/migrations/20211027083332-program_planner_fields.js
+++ packages/db/migrations/20211027083332-program_planner_fields.js
@@ -8,10 +8,10 @@ module.exports = {
       "program_log_id",
       "planner_id"
     );
-    await queryInterface.changeColumn("program_planner", "budgets", {
-      allowNull: true,
-      type: Sequelize.JSON,
-    });
+    // await queryInterface.changeColumn("program_planner", "budgets", {
+    //   allowNull: true,
+    //   type: Sequelize.JSON,
+    // });
     return await queryInterface.removeColumn("program_planner", "program_id");
   },
 
