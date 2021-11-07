568ae8c82655cbceb5b386c97a72e82bf7ed06cf
Krishna Verma <kvarma@kvarma-ltm.internal.salesforce.com>
2021-11-07 19:13:50 +0530

    fix build

---
+++ src/components/PlanningView/Activities.js
@@ -0,0 +1,77 @@
+import React, { Fragment, useEffect, useState } from "react";
+
+export default function Activities(props) {
+  const [activites, setActivities] = useState([]);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    let allActivites = {};
+    props.activities.forEach((activity) => {
+      if (allActivites[activity.formatId.label] === undefined) {
+        allActivites[activity.formatId.label] = [activity];
+      } else {
+        allActivites[activity.formatId.label].push(activity);
+      }
+    });
+    // for (let x in allActivites) {
+    //   console.log(x, allActivites[x]);
+    // }
+    setActivities(allActivites);
+  }, []);
+  return (
+    <div>
+      <ul className="list-activities">
+        {Object.keys(activites)
+          .splice(0, props.all ? Object.keys(activites).length : 5)
+          .map((item, k) => (
+            <Fragment>
+              <div
+                style={{
+                  marginBottom: "5px",
+                  marginTop: "5px",
+                }}
+              >
+                <span
+                  onClick={() => {
+                    setOpen(open !== k ? k : false);
+                  }}
+                  style={{
+                    backgroundColor: "#fcba03",
+                    cursor: "pointer",
+                    color: "white",
+                    fontSize: "small",
+                    padding: "5px",
+                    fontWeight: "bold",
+                    width: "auto",
+                    borderRadius: "5px",
+                  }}
+                >
+                  {item} - {activites[item].length}
+                </span>
+              </div>
+
+              {open === k &&
+                activites[item].map((format) => (
+                  <li className="small">
+                    {format.title} - {format.date}
+                  </li>
+                ))}
+            </Fragment>
+          ))}
+
+        {!props.all && props.activities.length > 4 ? (
+          <div
+            onClick={() => {
+              props.toggleModal();
+            }}
+            className="text-decoration-underline"
+          >
+            ...more
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </ul>
+    </div>
+  );
+}
