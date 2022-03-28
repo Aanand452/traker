c8955163220882ba7d559aa0387edb3af271c2e8
Krishna Verma <kvarma@kvarma-ltm.internal.salesforce.com>
2022-03-28 08:52:21 +0530

    Change from format to category

--- src/components/ActivitiesTable/index.js
+++ src/components/ActivitiesTable/index.js
@@ -2424,7 +2424,7 @@ class Table extends Component {
                   isOpen={this.state.isFormatFilterOpen}
                   labels={{
                     label: "",
-                    placeholder: "Catagory",
+                    placeholder: "Category",
                   }}
                   events={{
                     onChange: (event, { value }) => {
